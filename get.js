2e802bc4474155278db60a3840fe24594452d80e
wartakita <161806668+wartakita@users.noreply.github.com>
2024-06-29 11:59:56 +0700

    Update get.js

--- get.js
+++ get.js
@@ -1,51 +1,23 @@
-// Import library CryptoJS
-const CryptoJS = require("crypto-js");
-
 // Fungsi untuk mendapatkan parameter dari URL
 function getParameterByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, '\\$&');
-    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
-    const results = regex.exec(url);
+    let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-// Kunci AES Anda (contoh, harus disimpan secara aman dan tidak diungkapkan)
-const secretKey = "kunciRahasiaAnda123";
-
-// Fungsi untuk melakukan enkripsi AES
-function encryptAES(data, key) {
-    return CryptoJS.AES.encrypt(data, key).toString();
+// Fungsi untuk decode Base64
+function decodeBase64(str) {
+    return decodeURIComponent(escape(atob(str)));
 }
 
 // Mengambil parameter 'src' dari URL
 const srcParam = getParameterByName('src');
 
-// Jika parameter 'src' ada, decrypt dan setel sebagai atribut src dari iframe
+// Jika parameter 'src' ada, decode dan setel sebagai atribut src dari iframe
 if (srcParam) {
-    const decodedSrc = decryptAES(srcParam, secretKey);
-    if (decodedSrc) {
-        const iframe = document.getElementById('main-iframe');
-        if (iframe) {
-            iframe.src = decodedSrc;
-        } else {
-            console.error('Elemen dengan id "main-iframe" tidak ditemukan.');
-        }
-    } else {
-        console.error('Parameter src tidak valid.');
-    }
-} else {
-    console.error('Tidak ada parameter src yang ditemukan di URL.');
-}
-
-// Fungsi untuk melakukan dekripsi AES
-function decryptAES(ciphertext, key) {
-    try {
-        const bytes  = CryptoJS.AES.decrypt(ciphertext, key);
-        return bytes.toString(CryptoJS.enc.Utf8);
-    } catch (e) {
-        console.error('Gagal melakukan dekripsi AES:', e);
-        return null;
-    }
+    const decodedSrc = decodeBase64(srcParam);
+    document.getElementById('main-iframe').src = decodedSrc;
 }
